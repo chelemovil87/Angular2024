6ed8b52c8379acdfc2bf910c07fe10dddac5dfb2
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-12 14:57:17 +0100

    first commit

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,20 @@
+
+
+
+const name = 'Strider'; 
+
+let hpPoints: number | 'full' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'full';
+
+console.log({
+    name, hpPoints, isAlive
+});
+
+
+
+
+
+
+export{};
