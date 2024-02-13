96e212c41adb695c39bdce909e72a80d8ee45c14
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-13 12:30:09 +0100

    Agregado desestructuración de funciones

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,69 @@
+interface Product{
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: "Nokia A1",
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: "Ipad Air",
+    price: 250.0
+}
+
+interface TaxCalculationOptions{
+    tax: number;
+    product: Product[];
+}
+
+//! Los comentados es la implementacion de la funcion sin destructuring
+// function taxCalculation(options: TaxCalculationOptions):[number,number]{
+    function taxCalculation(options: TaxCalculationOptions):[number,number]{
+        
+        const {tax, product} = options;
+        let total = 0;
+        
+        product.forEach(({price}) => {
+    //  product.forEach( product => {
+    //  options.product.forEach(({price}) => {
+        
+        total += price;
+    //  total += product.price;
+    });
+    return [total, total * tax];
+    // return [total, total * options.tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+
+//! Los comentados es la implementacion de la funcion sin destructuring
+const [total, taxTotal] = taxCalculation({
+// const result = taxCalculation({
+    product: shoppingCart,
+    tax: tax, 
+});
+
+console.log('Total:', total);
+// console.log('Total:', result [0]);
+console.log('Tax:', taxTotal);
+// console.log('Tax:', result[1];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export{};
