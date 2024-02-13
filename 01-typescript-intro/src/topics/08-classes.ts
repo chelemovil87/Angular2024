fa0ffaec27b45f06c0ddb6a890241b2ebbc5ec58
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-13 13:53:18 +0100

    update classes

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,14 +1,45 @@
-export class Person {
-    public name: string;
-    private address: string;
+//* Los comentarios en la clase son para que se aprecie el standar en el constructor
 
-    constructor() {
+export class Person {
+    // public name: string;
+    // private address: string;
+
+    constructor(
+        public firtname: string,
+        public lastnme: string, 
+        private address: string = 'No address'
+        ) {}
+
+    /*constructor() {
         this.name = 'Jose';
         this.address = 'Cordoba';
-    }
+    }*/
 
 }
+//!En este comentario se puede ver que la clase Hero  hereda de la clase Person
+/* export class Hero extends Person{
 
-const iroman = new Person();
+   constructor(
+       public alterEgo: string,
+       public age: number,
+       public realName: string
+   ) {
+       super(realName, 'Cordoba');
+     }
+ }*/
+export class Hero {
 
-console.log('iroman', iroman);
+   // public person: Person;
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person,
+    ) {}
+}
+
+const tony = new Person('Tony', 'Stark', 'Cordoba');
+const iroman = new Hero('Hulk', 36 ,'Jose', tony);
+
+console.log (iroman);
