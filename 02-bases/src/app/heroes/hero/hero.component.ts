101b3952313fb8d84bb525ac72f531dee7b83c94
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-14 17:39:01 +0100

    interpolacion

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
+
+  public name: string = 'iroman';
+  public age: number = 45;
 
 }
