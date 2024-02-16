17e1a36fc0e50d90d356e749b6d01f3baa3a1fd1
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-16 17:48:14 +0100

    FormModule and ngModel, update secctions

--- 02-bases/src/app/dbz/pages/main-pages.component.ts
+++ 02-bases/src/app/dbz/pages/main-pages.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,input } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent   {
+
+  public  characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000
+    },
+    {
+      name: 'Goku',
+      power: 9500
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    },
+    {
+      name: 'Trunks',
+      power: 6000
+    },
+    {
+      name: 'Piccolo',
+      power: 5000
+    },
+    {
+      name: 'Gohan',
+      power: 4000
+    },
+    {
+      name: 'Goten',
+      power: 3000
+    },
+    {
+      name: 'Yamcha',
+      power: 2000
+    },
+    {
+      name: 'Tenshinhan',
+      power: 1000
+    },
+    {
+      name: 'Chaoz',
+      power: 500
+    }
+  ];
 
 }
