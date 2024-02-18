8f01077a2b1825fd518e3eeda30001b90e133189
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-18 17:38:13 +0100

    gifsService and pipes

--- 03-gifs-app/src/app/gifs/components/search-box/search-box-component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box-component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
+import { GifsService } from '../../services/gifs.service';
 @Component({
   selector: 'gifs-search-box',
   template: `
@@ -18,11 +18,12 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() {}
+  constructor(private gifsService: GifsService) {}
 
   searchTag( ) {
     const newTag = this.tagInput.nativeElement.value;
-    console.log(newTag)
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
   }
 
 }
