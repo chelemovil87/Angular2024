e9b7d43ceb28fcdaddecff11745a472829aaf9c0
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-19 17:47:15 +0100

    lazy loading and animatedcss ||  End app

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
+import { CardComponent } from './components/card/card.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { HomePagesComponent } from './pages/home-pages/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box-component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { CardComponent } from './components/card/card.component';
-
-
 
 @NgModule({
   declarations: [
-    HomePagesComponent,
-    SearchBoxComponent,
+    CardComponent,
     CardListComponent,
-    CardComponent
+    HomePagesComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule,
+    SharedModule
   ],
   exports: [
     HomePagesComponent,
