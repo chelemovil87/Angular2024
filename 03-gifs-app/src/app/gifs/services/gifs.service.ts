ae1e1364dd5f6c954daf1e5a1f53fe0851d82cee
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-19 13:01:16 +0100

    Apikey and http data

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+// const GIPHI_API_KEY = '8mhVOjXZ9EgBi9wj1OAAuygY9e9qXUIJ';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
-  constructor() { }
+  private apiKey:       string = '8mhVOjXZ9EgBi9wj1OAAuygY9e9qXUIJ';
+  private serviceUrl:   string = 'http://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) { }
 
   get tagHistory() {
     return [...this._tagsHistory];
@@ -20,10 +29,20 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  public searchTag( tag: string ): void {
+  public searchTag( tag: string ):void {
     if(tag.length === 0) return;
     this.organizeHistory(tag);
-    if (tag.length === 0) return;
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search?`,{params})
+      .subscribe (resp => {
+        this.gifList = resp.data;
+      console.log({gifs: this.gifList});
+      });
   }
 
 }
