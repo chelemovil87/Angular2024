123a3b9bf4a21f7df31148092a319a0587b8f8e9
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-20 15:08:04 +0100

    Upgrade to angular@16 to fix lazy loading issues with loadChildren

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -5,23 +5,11 @@ import { AboutPageComponent } from './shared/pages/about-page/about-page.compone
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomePageComponent,
-  },
-  {
-    path: 'about',
-    component: AboutPageComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactPageComponent,
-  },
-
-  {
-    path: '**',
-    redirectTo: '',
-  }
+  {path: '',component: HomePageComponent,},
+  {path: 'about',component: AboutPageComponent,},
+  {path: 'contact',component: ContactPageComponent,},
+  {path: 'countries',loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
--- 04-countryApp/src/app/shared/pages/contact-page/contact-page.component.ts
+++ 04-countryApp/src/app/shared/pages/contact-page/contact-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-contact',
   templateUrl: './contact-page.component.html',
-  styles: ``
 })
 export class ContactPageComponent {
 
