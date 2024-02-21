88aa2e8395e3c94a705e5129884a14baf649b047
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2024-02-21 13:46:12 +0100

    update pages countries

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
-export class ServiceNameService {
+
+export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1/';
 
@@ -17,6 +18,26 @@ export class ServiceNameService {
 
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(err => of([]))
+    );
   }
 
+  searchCountry( term: string): Observable<Country[]>{
+
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(err => of([]))
+    );
+  }
+
+  searchRegion( region: string): Observable<Country[]>{
+
+      const url = `${this.apiUrl}/region/${region}`;
+      return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(err => of([]))
+      );
+    }
 }
